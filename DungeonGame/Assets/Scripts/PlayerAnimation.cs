901a5eb2cd7165883cda9931d5a17ff8d491ba58
Bruno Tiago S. C <116372081+brunort5@users.noreply.github.com>
2023-05-24 06:20:57 -0300

    DungeonGame

--- DungeonGame/Assets/Scripts/PlayerAnimation.cs
+++ DungeonGame/Assets/Scripts/PlayerAnimation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GlobalConstants;
-using Unity.VisualScripting;
+
 
 public class PlayerAnimation : MonoBehaviour
 {
